5aeaa7291b75180911b008dda858993dae60fc4b
elvin <elvin.azim01@gmail.com>
2022-04-02 11:35:06 +0400

    If

---
+++ Python/Lesson2/main.py
@@ -0,0 +1,48 @@
+# <editor-fold desc="concatenation&interpolation">
+
+# num1 = '5'
+# num2 = '6'
+#
+# print(num1 + num2)  # String concatenation
+# print(f"{num1}{num2}")  # String interpolation
+# </editor-fold>
+
+# <editor-fold desc="SEP&END">
+
+# print("Hello", "World", sep=',')    # separate, default value is ' '
+# sep - keyword argument
+# 'Hello' - value argument
+
+# print("Hello", "Dear", sep='❤', end='\t')  # ending, default value of end is '\n'
+# print("World")
+# print("Batman")
+# </editor-fold>
+
+# <editor-fold desc="Operators">
+# Logical ==, !=, >, <, >=, <=
+# Math +, -, *, /, **, //, %
+# Conditional and, or
+# </editor-fold>
+
+# <editor-fold desc="If conditions">
+
+# num1 = 5
+# num2 = 10
+# num3 = 15
+#
+# if num1 < num2 < num3:
+#     print("Hello")
+
+# num1 = 1
+# num2 = 0
+#
+# if num1 % 2 == 0 and num2 > 0:
+#     print(f"{num1} is Even and {num2} is bigger than 0")
+# elif num1 and not num2:
+#     print(num1)
+# else:
+#     print("Error")
+
+# </editor-fold>
+
+
