2d429dd18b9d2731bca1b65b2c52d320bc1c8cd4
e.azimov <elvin.azim01@gmail.com>
2022-04-09 12:29:05 +0400

    Lesson7

---
+++ Python/Lesson7/main.py
@@ -0,0 +1,72 @@
+# num1 = 5
+# num2 = 6
+#
+# if num1 % 2 == 0:
+#     print("Even")
+# if num1 > 0:
+#     print("Positive")
+# if num2 == 6:
+#     print(num2)
+# if num1 and num2:
+#     print(num1, num2)
+
+# name = "Elvin"
+
+
+# for char in name:
+#     print(char)
+
+# a = iter(name)
+# char = a.__next__()
+# print(char)
+# char = a.__next__()
+# print(char)
+# char = a.__next__()
+# print(char)
+# char = a.__next__()
+# print(char)
+# char = a.__next__()
+# print(char)
+
+# for i in range(len(name)):
+#     print(name[i])
+
+
+# text = "p  op"
+#
+# if text.replace(' ', '').lower() == text[::-1].replace(' ', '').lower():
+#     print("Yes")
+# else:
+#     print("No")
+
+# no_space_text = text.replace(" ", "")
+# reversed_text = ""
+#
+# for i in range((len(no_space_text) - 1), -1, -1):
+#     reversed_text += no_space_text[i]
+#
+# if text == reversed_text:
+#     print("YES")
+# else:
+#     print("NOT")
+
+
+# name = "Elvin"
+
+# print(name[0])
+# print(name[0:3])
+# print(name[0:])
+# print(name[0::3])
+# print(name[::-1])
+
+# name = "Elvin Azimov"
+#
+# a = name.split()
+#
+# print(a)
+
+
+name = "Elvin"
+
+name = name.split()
+print(name)
