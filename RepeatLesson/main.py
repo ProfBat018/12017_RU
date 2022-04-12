f823dbe804a0236dda3e60eea07c2a51badb3b36
elvin <elvin.azim01@gmail.com>
2022-04-12 19:26:41 +0400

    repeat lesson

---
+++ RepeatLesson/main.py
@@ -0,0 +1,13 @@
+# text = "Something in the way"
+#
+# print(text[2:5])
+# print(text[2::5])
+# print(text[:3])
+# print(text[::-1])
+
+# print(int(str("")) + int(str("Elvin")))
+
+# print(bool("") + bool("Elvin"))
+
+# print(str(bool("")) + str(bool("Elvin")))
+
